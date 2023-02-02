b1ee6bfdeb28ce76355e6df9ae8ecb59f5477978
Chillie <dev@celiadelaire.com>
2023-02-02 12:28:40 +0100

    feat: generate ssr host and remote apps
    With command `npx nx g host dashboard --remotes=login --ssr`

---
+++ apps/login/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-nx-mf-ssr-root',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent {}
---
+++ apps/login/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-nx-mf-ssr-login-entry',
+  template: `<angular-nx-mf-ssr-nx-welcome></angular-nx-mf-ssr-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
